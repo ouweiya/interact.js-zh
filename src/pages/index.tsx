93431d035085041c1455bb5d4223133255587a27
ouweiya <higogle1@gmail.com>
2024-12-24 09:30:13 +0800

    移除首页标题背景颜色样式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
     return (
-        <header className={clsx(styles.heroBanner)} style={{background: '#e9f6ff'}}>
+        <header className={clsx(styles.heroBanner)} >
             <div className="container">
                 <Heading as="h1" className="hero__title">
                     {siteConfig.title}
